473ea7cb9f03ca97993ba3f7a86f32f64983a71c
Thakshara Dhananjaya <thakshara@surge.global>
2024-08-19 15:53:00 +0530

    Add attributes to user entity

--- internal/domain/user/entity.go
+++ internal/domain/user/entity.go
@@ -1,8 +1,12 @@
 package user
 
+import "time"
+
 type User struct {
     ID    uint   `gorm:"primaryKey"`
     Name  string `gorm:"size:255;not null"`
     Email string `gorm:"size:255;unique;not null"`
     Age   int    `gorm:"not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+    UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
